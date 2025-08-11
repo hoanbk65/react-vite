2ed9b3a8359bcb4fe7ae7f805a99152265898649
Hoan <ledanghoan1312@gmail.com>
2025-08-11 16:06:18 +0700

    vid30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,18 @@
 // () => { }
-  // component =html +css +js
+// component =html +css +js
+// JSX : 1 parent
+// fragment
+import "./style.css";
 
 const MyComponent = () => {
-  return(
-    <div> eric & hoidanit update</div> // tag
-  )
-}
+  return (
+    <>
+      <div> eric & hoidanit update</div> // tag
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
+};
 
 export default MyComponent;
